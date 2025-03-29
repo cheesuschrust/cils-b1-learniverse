e9b4a535fd32af202cd90febeea7afd0d01a2d22
cheesuschrust <sneffrey@msn.com>
2025-03-29 16:26:39 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Add this import
 import App from './App';
 import './index.css';
 import './styles/flashcard.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Add this wrapper */}
+      <App />
+    </BrowserRouter> {/* Close the wrapper */}
   </React.StrictMode>
 );
