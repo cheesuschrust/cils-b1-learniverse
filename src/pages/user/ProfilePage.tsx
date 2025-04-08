4c2069d8e1c14900feacfbf34fb5360ded49f348
cheesuschrust <sneffrey@msn.com>
2025-04-08 11:00:07 -0500

    Create ProfilePage.tsx

---
+++ src/pages/user/ProfilePage.tsx
@@ -0,0 +1,13 @@
+// ProfilePage.tsx
+import React from 'react';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Profile Page</h1>
+      <p>This is where user profile information will be displayed.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
