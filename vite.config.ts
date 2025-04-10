235f27b9a3461cc42a12861e44933288c87d163c
cheesuschrust <sneffrey@msn.com>
2025-04-10 09:47:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';  
 import react from '@vitejs/plugin-react';  
 import * as path from 'path';  
+import { componentTagger } from 'lovable-tagger';
 
 export default defineConfig(({ mode }) => {
   return {
@@ -10,7 +11,8 @@ export default defineConfig(({ mode }) => {
         jsxRuntime: 'automatic',
         jsxImportSource: 'react',
       }),
-    ],
+      mode === 'development' && componentTagger(),
+    ].filter(Boolean),
     
     // Server Configuration  
     server: {  
